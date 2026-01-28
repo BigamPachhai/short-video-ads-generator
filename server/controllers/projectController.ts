f053b6b914156c40118b6cc32cd42d004d77aa1f
Bigam Pachhai <bigampachhai2@gmail.com>
2026-01-28 23:57:29 +0545

    feat: add Google GenAI integration and update dependencies
    - Added @google/genai dependency to package.json
    - Created ai.ts configuration file for Google GenAI with API key setup

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -2,6 +2,23 @@ import { Request, Response } from "express";
 import * as Sentry from "@sentry/node";
 import { prisma } from "../configs/prisma.js";
 import { v2 as cloudinary } from "cloudinary";
+import {
+  GenerateContentConfig,
+  HarmBlockThreshold,
+  HarmCategory,
+} from "@google/genai";
+import fs from "fs";
+import path from "path";
+import ai from "../configs/ai.js";
+
+const loadImage = (path: string, mimeType: string) => {
+  return {
+    inlineData: {
+      data: fs.readFileSync(path).toString("base64"),
+      mimeType,
+    },
+  };
+};
 
 export const createProject = async (req: Request, res: Response) => {
   let tempProjectId: string;
@@ -21,11 +38,9 @@ export const createProject = async (req: Request, res: Response) => {
     const images: any = req.files;
 
     if (!images || images.length < 2 || !productName) {
-      return res
-        .status(400)
-        .json({
-          message: "Please upload at least 2 images and provide product name",
-        });
+      return res.status(400).json({
+        message: "Please upload at least 2 images and provide product name",
+      });
     }
 
     const user = await prisma.user.findUnique({ where: { id: userId } });
@@ -69,7 +84,86 @@ export const createProject = async (req: Request, res: Response) => {
     });
     tempProjectId = product.id;
 
-    res.status(201).json({ project: product });
+    const model = "gemini-3-pro-image-preview";
+
+    const generationConfig: GenerateContentConfig = {
+      maxOutputTokens: 32768,
+      temperature: 1,
+      topP: 0.95,
+      responseModalities: ["image"],
+      imageConfig: {
+        aspectRatio: aspectRatio || "9:16",
+        imageSize: "1K",
+      },
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.OFF,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.OFF,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.OFF,
+        },
+      ],
+    };
+
+    //images to base64 structure for ai model
+
+    const img1base64 = loadImage(images[0].path, images[0].mimeType);
+    const img2base64 = loadImage(images[1].path, images[1].mimeType);
+
+    const prompt = {
+      text: `Combine the person and product into relastic photo ${userPrompt}`,
+    };
+
+
+
+//generate the image using ai model
+const response:any=await ai.models.generateContent({
+    model,
+    contents:[img1base64,img2base64,prompt],
+    config:generationConfig,
+
+})
+
+if(!response?.candidates?.[0]?.content?.parts){
+    throw new Error("No image generated")
+}
+
+const parts=response.candidates[0].content.parts;
+let finalBuffer:Buffer | null=null;
+
+for(const part of parts){
+    if(part.inlineData){
+        finalBuffer=Buffer.from(part.inlineData.data,"base64")
+    }
+}
+
+if(!finalBuffer){
+    throw new Error("Failed to generate image")
+
+}
+
+const base64Image=`data:image/png;base64,${finalBuffer.toString("base64")}`
+
+const uploadResult=await cloudinary.uploader.upload(base64Image,{
+    resource_type:"image"})
+
+
+await prisma.project.update({
+    where:{id:product.id},
+    data:{
+        generatedImage:uploadResult.secure_url,
+        isGenerating:false,
+    }
+})
+
+
+    res.status(201).json({ projectId: product.id });
   } catch (error: any) {
     // If credit was deducted but project creation failed, refund the credits
     if (isCreditDeducted) {
@@ -89,7 +183,6 @@ export const createProject = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
-
 
 export const createVideo = async (req: Request, res: Response) => {
   try {
